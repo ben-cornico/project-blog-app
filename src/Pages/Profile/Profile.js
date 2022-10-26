7f12493c98738dea243a53816729366e969106ba
CarLoWorld <ben.carlo.cornico@gmail.com>
2022-10-26 00:17:38 -0700

    adding blog list design

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Profile.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import testImg from '../../img/test.jpg'
 
 const Profile = () => {
   return (
@@ -42,7 +43,70 @@ const Profile = () => {
                 </ul>
             </div>
             <div className="main">
-                MAIN
+                <div className="profile-home">
+                    <div className="blog-card">
+                        <p className="timeago">2 days ago</p>
+                        
+                        <p className="card-title">Lorem Ipsum</p>
+
+                        <div className="card-body">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero in commodi necessitatibus omnis. Voluptatum tenetur obcaecati quidem qui nesciunt provident unde, nihil quaerat? Iusto illum omnis quae nisi amet voluptate.
+                            Provident, magnam eveniet error repellendus beatae sapiente amet optio odio repudiandae totam animi, sunt reiciendis, officia nobis aliquam ad libero! Recusandae laboriosam quibusdam dolorem nostrum a saepe est, ab non.
+                            Libero ullam vel alias laboriosam eum cupiditate? Magnam, corporis aliquam possimus laudantium, blanditiis dolores perspiciatis repellendus enim, vero consequatur ipsa inventore? Reprehenderit nisi amet quam perferendis animi obcaecati magni quibusdam.
+                            Odio deserunt reprehenderit distinctio sint aliquam quam perferendis molestiae tempore laboriosam itaque similique quibusdam esse molestias atque, obcaecati velit in cupiditate doloremque. Maiores, distinctio expedita! Corrupti odio similique ab maiores!
+                            Corporis expedita, eum quidem perspiciatis, fugit magni, a officiis ratione aut nostrum eius mollitia nihil nobis impedit inventore! Libero dicta ex soluta quia! Ducimus sunt quis fugit molestias, cumque nemo?
+                        </div>
+
+                        <div className="card-footer">
+                            <p className="timetoread">5 minutes to read</p>
+                            <span className='mdi mdi-bookmark'></span>
+                        </div>
+                        
+                        <img src={testImg} alt="" srcset="" />
+                    </div>
+
+                    <div className="blog-card">
+                        <p className="timeago">2 days ago</p>
+                        
+                        <p className="card-title">Lorem Ipsum</p>
+
+                        <div className="card-body">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero in commodi necessitatibus omnis. Voluptatum tenetur obcaecati quidem qui nesciunt provident unde, nihil quaerat? Iusto illum omnis quae nisi amet voluptate.
+                            Provident, magnam eveniet error repellendus beatae sapiente amet optio odio repudiandae totam animi, sunt reiciendis, officia nobis aliquam ad libero! Recusandae laboriosam quibusdam dolorem nostrum a saepe est, ab non.
+                            Libero ullam vel alias laboriosam eum cupiditate? Magnam, corporis aliquam possimus laudantium, blanditiis dolores perspiciatis repellendus enim, vero consequatur ipsa inventore? Reprehenderit nisi amet quam perferendis animi obcaecati magni quibusdam.
+                            Odio deserunt reprehenderit distinctio sint aliquam quam perferendis molestiae tempore laboriosam itaque similique quibusdam esse molestias atque, obcaecati velit in cupiditate doloremque. Maiores, distinctio expedita! Corrupti odio similique ab maiores!
+                            Corporis expedita, eum quidem perspiciatis, fugit magni, a officiis ratione aut nostrum eius mollitia nihil nobis impedit inventore! Libero dicta ex soluta quia! Ducimus sunt quis fugit molestias, cumque nemo?
+                        </div>
+
+                        <div className="card-footer">
+                            <p className="timetoread">5 minutes to read</p>
+                            <span className='mdi mdi-bookmark'></span>
+                        </div>
+                        
+                        <img src={testImg} alt="" srcset="" />
+                    </div>
+
+                    <div className="blog-card">
+                        <p className="timeago">2 days ago</p>
+                        
+                        <p className="card-title">Lorem Ipsum</p>
+
+                        <div className="card-body">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero in commodi necessitatibus omnis. Voluptatum tenetur obcaecati quidem qui nesciunt provident unde, nihil quaerat? Iusto illum omnis quae nisi amet voluptate.
+                            Provident, magnam eveniet error repellendus beatae sapiente amet optio odio repudiandae totam animi, sunt reiciendis, officia nobis aliquam ad libero! Recusandae laboriosam quibusdam dolorem nostrum a saepe est, ab non.
+                            Libero ullam vel alias laboriosam eum cupiditate? Magnam, corporis aliquam possimus laudantium, blanditiis dolores perspiciatis repellendus enim, vero consequatur ipsa inventore? Reprehenderit nisi amet quam perferendis animi obcaecati magni quibusdam.
+                            Odio deserunt reprehenderit distinctio sint aliquam quam perferendis molestiae tempore laboriosam itaque similique quibusdam esse molestias atque, obcaecati velit in cupiditate doloremque. Maiores, distinctio expedita! Corrupti odio similique ab maiores!
+                            Corporis expedita, eum quidem perspiciatis, fugit magni, a officiis ratione aut nostrum eius mollitia nihil nobis impedit inventore! Libero dicta ex soluta quia! Ducimus sunt quis fugit molestias, cumque nemo?
+                        </div>
+
+                        <div className="card-footer">
+                            <p className="timetoread">5 minutes to read</p>
+                            <span className='mdi mdi-bookmark'></span>
+                        </div>
+                        
+                        <img src={testImg} alt="" srcset="" />
+                    </div>
+                </div>
             </div>
             <div className="profile-info">
                 info
